73d9136f95936c00fe8b34d6c7bd7c9e5ac130ce
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2024-12-30 13:01:01 +0200

    done

---
+++ observer.js
@@ -0,0 +1,25 @@
+
+const imagesArr = document.querySelectorAll('.img')
+const observer = new IntersectionObserver(observerImages)
+
+function loadImage(image) {
+    const dataSrc = image.getAttribute('data-src')
+    image.src = dataSrc
+    image.classList.add('animation-image')
+}
+
+function observerImages(imagesArr) {
+    imagesArr.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            loadImage(entry.target)
+            observer.unobserve(entry.target)
+        }
+    });
+}
+
+imagesArr.forEach((imageEl) => {
+    observer.observe(imageEl)
+    imageEl.classList.remove('animation-image')
+})
+
